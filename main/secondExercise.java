9fa0aeb01b206b3d76eccd38090194993f5ce05e
gabrielgflower <gabrielgirardi.sl@gmail.com>
2021-10-23 20:52:58 -0300

    Adding second exercise

---
+++ main/secondExercise.java
@@ -0,0 +1,24 @@
+public class secondExercise {
+    public String name;
+    public int age;
+    public String city;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String n) {
+        this.name = n;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int a) {
+        this.age = a;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String c) {
+        this.city = c;
+    }
+}
